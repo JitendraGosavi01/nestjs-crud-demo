31eba6d5ca4c885fc932119ee93a8cf346350b05
Jitendra Gosavi <jitendra_gosavi@epam.com>
2022-06-14 18:31:08 +0530

    CRUD operations  with mongo database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './Users/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://Jeet:Jeet%401991@freecluster.ehj5s.mongodb.net/nestjs-demo?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
